a97e4191dbb26b708ca07c476768d31e51420ed1
AdamKorb <156099409+AdamKorb@users.noreply.github.com>
2024-01-28 16:18:18 +0100

    Add files via upload

---
+++ my-task-app/src/data.js
@@ -0,0 +1,12 @@
+const data = [
+    { id: 1, name: 'Komponentom robiť templaty' },
+    { id: 2, name: 'Komponenty vnárať pod iné komponenty' },
+    { id: 3, name: 'Routy s pagami' },
+    { id: 4, name: 'Iteracia cez zoznamy v template' },
+    { id: 5, name: 'className' },
+    { id: 6, name: 'Import className from css' },
+    { id: 7, name: 'Props' },
+    { id: 8, name: 'Podmienene vnorenie v template' },
+]
+
+export default data
